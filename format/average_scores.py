14adc148c206402bda7e4919c3da02d3ddaaa5bc
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-02-02 15:41:18 -0600

    Finished

--- format/average_scores.py
+++ format/average_scores.py
@@ -5,12 +5,15 @@ def average():
     score1 = input("Enter score #1: ")
     score2 = input("Enter score #2: ")
     score3 = input("Enter score #3: ")
-    average = int((int(score1) + int(score2) + int(score3)) / 3)
-    print(average)
+    return float(score1) + float(score2) + float(score3) / 3
 
 
 def main():
-    average()
+    first_name = input("First name: ")
+    last_name = input("Last name: ")
+    age = int(input("Age: "))
+    average_score = average()
+    print("%s, %s age: %d average grade: %.2f" % (last_name, first_name, age, average_score))
 
 
 if __name__ == '__main__':
